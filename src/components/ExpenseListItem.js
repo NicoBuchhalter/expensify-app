67851bb9df20b3060c2c34b6dfb467ea762b815d
NicoBuchhalter <nico.buch.1010@gmail.com>
2019-05-26 14:16:26 -0300

    Expensify App

---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>Amount: $ {parseFloat(amount / 100)}</p>
+    <p>Created at: {createdAt}</p>
+  </div>
+); 
+
+
+export default ExpenseListItem;
