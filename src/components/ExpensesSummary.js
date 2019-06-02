0ff32193f79c24c8121b41767ccd44eb73708b3e
NicoBuchhalter <nico.buch.1010@gmail.com>
2019-06-02 15:45:05 -0300

    Small expenses summary fixes

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,11 +4,15 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
-  <div>
-    <h1>Viewing <b>{props.expenseCount}</b> expense{props.expenseCount !== 1 && 's'} totalling <b>{numeral(props.expensesTotal / 100).format('$0,0.00')}</b></h1>
-  </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedTotal}</h1>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const expenses = selectExpenses(state.expenses, state.filters)
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import {ExpensesSummary} from '../../components/ExpensesSummary';
-import expenses from '../fixtures/expenses';
 
 test('Should show correct message when only one expense', () => {
   expect(shallow(<ExpensesSummary expenseCount={1} expensesTotal={15000}/>)).toMatchSnapshot();
